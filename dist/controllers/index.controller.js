95c5f4fed9adb61b970972ae113360e16da7acb3
José Miranda <84145112+Josemiranda989@users.noreply.github.com>
2022-11-04 15:20:43 -0300

    Delete dist directory

--- dist/controllers/index.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.indexWelcome = void 0;
-function indexWelcome(req, res) {
-    return res.json("welcome to my API");
-}
-exports.indexWelcome = indexWelcome;
